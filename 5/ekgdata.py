b306f43767ad4be9fad64d5970c076c7d14dbc0c
svenjamariko <svenja.taft@icloud.com>
2024-06-03 12:02:57 +0200

    data

---
+++ 5/ekgdata.py
@@ -0,0 +1,107 @@
+import json
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+# %% Objekt-Welt
+
+# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+
+class EKGdata:
+
+    # Konstruktor der Klasse soll die Daten einlesen
+    def __init__(self, ekg_dict):
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
+        self.df["Time in s"] = self.df["Time in ms"] / 1000  # Convert milliseconds to seconds
+
+    @staticmethod
+    def load_by_id(ekg_id):
+        file = open("data/person_db.json")
+        person_data = json.load(file)
+        for person in person_data:
+            ekg_tests = person['ekg_tests']
+            for ekg in ekg_tests:
+                if ekg["id"] == ekg_id:
+                    return ekg
+        return None
+
+    @staticmethod
+    def find_peaks(series, threshold, respacing_factor=5):
+        # Respace the series
+        series = series.iloc[::respacing_factor]
+        # Filter the series
+        series = series[series > threshold]
+
+        peaks = []
+        last = 0
+        current = 0
+        next = 0
+
+        for index, row in series.items():
+            last = current
+            current = next
+            next = row
+
+            if last < current and current > next and current > threshold:
+                peaks.append(index - respacing_factor)
+
+        return peaks
+
+    @staticmethod
+    def estimate_hr(peaks):
+        peak_times_sec = np.array(peaks) / 1000
+        rr_intervals = np.diff(peak_times_sec)
+        heart_rate_at_peaks = 60 / rr_intervals
+        heart_rate_times = peak_times_sec[1:]
+        return heart_rate_times, heart_rate_at_peaks
+
+    @staticmethod
+    def make_ekg_plot(peaks, df):
+        fig = px.line(df, x="Time in s", y='EKG in mV')
+        fig.add_trace(go.Scatter(x=df["Time in s"].iloc[peaks], y=df["EKG in mV"].iloc[peaks], mode='markers', name='Peaks', marker=dict(color='red', size=8)))
+        fig.update_layout(title='EKG over Time', xaxis_title='Time in s', yaxis_title='EKG in mV')
+        return fig
+    
+    @staticmethod
+    def make_hf_plot(heart_rate_times, heart_rate_at_peaks):
+        fig = px.line(x=heart_rate_times, y=heart_rate_at_peaks)
+        fig.update_layout(title='Heart Rate over Time', xaxis_title='Time in s', yaxis_title='Heart Rate in bpm')
+        return fig
+
+# %% Testen der Funktionen
+
+if __name__ == "__main__":
+    # Load the JSON data
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    
+    # Create an instance of EKGdata
+    ekg = EKGdata(ekg_dict)
+    
+    # Load EKG data by ID
+    Ekg_1 = EKGdata.load_by_id(1)
+    
+    # Read EKG data from file
+    df = pd.read_csv(r'data/ekg_data/01_Ruhe.txt', sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
+    df["Time in s"] = df["Time in ms"] / 1000  # Convert milliseconds to seconds
+    
+    # Find peaks
+    peaks = EKGdata.find_peaks(df["EKG in mV"].copy(), 340, 5)
+    
+    # Estimate heart rate
+    heart_rate_times, heart_rate_at_peaks = EKGdata.estimate_hr(peaks)
+    
+    # Create and display the EKG plot
+    fig = EKGdata.make_ekg_plot(peaks, df)
+    fig.show()
+
+    # Create and display the heart rate plot
+
+    fig = EKGdata.make_hf_plot(heart_rate_times, heart_rate_at_peaks)
+    fig.show()
---
+++ 5/main.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import read_person_data
+import ekgdata
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+from person import Person
+import numpy as np
+import os
+
+def callback():
+    print(f"The user has changed to{st.session_state.aktuelle_versuchsperson} and {st.session_state.aktuelles_ekg}")
+
+# Zu Beginn
+
+# Lade alle Personen
+person_names = read_person_data.get_person_list(read_person_data.load_person_data())
+
+# Anlegen diverser Session States
+## Gewählte Versuchsperson
+if 'aktuelle_versuchsperson' not in st.session_state:
+    st.session_state.aktuelle_versuchsperson = 'None'
+
+## Anlegen des Session State. Bild, wenn es kein Bild gibt
+if 'picture_path' not in st.session_state:
+    st.session_state.picture_path = 'data/pictures/none.jpg'
+
+## Session State für Pfad zu EKG Daten 
+if 'ekg_path' not in st.session_state:
+    st.session_state.ekg_path = None
+
+# Design des Dashboards
+
+# Schreibe die Überschrift
+st.write("# EKG APP")
+st.write("## Versuchsperson auswählen")
+
+# Auswahlbox, wenn Personen anzulegen sind
+st.session_state.aktuelle_versuchsperson = st.selectbox(
+    'Versuchsperson',
+    options = person_names, key="select_versuchsperson")
+
+# Name der Versuchsperson
+st.write("Der Name ist: ", st.session_state.aktuelle_versuchsperson)
+
+# Date input slot
+current_person_dict = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+
+current_person = Person(current_person_dict)
+
+ekg_tests = current_person_dict["ekg_tests"]
+ekg_dates = [ekg["date"] for ekg in ekg_tests]
+
+st.write("## Datum auswählen")
+st.session_state.aktuelles_ekg = st.selectbox(
+    'EKG-Datum',
+    options = ekg_dates, key="sbEKGDatum",
+    on_change=callback)
+selected_ekg = [ekg for ekg in ekg_tests if ekg["date"] == st.session_state.aktuelles_ekg][0]
+
+
+st.write("Das ausgewählte Datum ist:", st.session_state.aktuelles_ekg)
+
+# create a new EKGdata object
+current_ekg = ekgdata.EKGdata(selected_ekg)
+st.session_state.ekg_path = current_ekg.data
+
+# TODO: Weitere Daten wie Geburtsdatum etc. schön anzeigen
+
+# Nachdem eine Versuchsperson ausgewählt wurde, die auch in der Datenbank ist
+# Finde den Pfad zur Bilddatei
+
+if os.path.exists(st.session_state.ekg_path):
+    st.write("EKG-Daten vorhanden")
+else:
+    st.write("EKG-Daten nicht vorhanden")
+
+st.write("Name der Versuchsperson: ", current_person.firstname, current_person.lastname, ":sunglasses:")
+st.write("Geburtsjahr: ", current_person.date_of_birth)
+st.write("Alter: ", current_person.age)
+st.write("Maximale Herzfrequenz :anatomical_heart: : ", current_person.calc_max_heart_rate())
+
+
+if st.session_state.aktuelle_versuchsperson in person_names:
+    person_data = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+    st.session_state.picture_path = person_data.get("picture_path", 'data/pictures/none.jpg')
+    st.session_state.ekg_path = person_data.get("ekg_path", 'data/ekg_data/01_Ruhe.txt')  # Use .get to handle missing keys
+
+# Bild anzeigen
+image = Image.open(st.session_state.picture_path)
+st.image(image, caption=st.session_state.aktuelle_versuchsperson)
+
+# Öffne EKG-Daten
+# TODO: Für eine Person gibt es ggf. mehrere EKG-Daten. Diese müssen über den Pfad ausgewählt werden können
+# Vergleiche Bild und Person
+if st.session_state.ekg_path is not None:
+
+    # Lade die EKG-Daten im df
+    df = pd.read_csv(st.session_state.ekg_path, sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
+    df["Time in s"] = df["Time in ms"] / 1000  # Convert milliseconds to seconds
+
+    # Detect the peaks
+    peaks = ekgdata.EKGdata.find_peaks(df["EKG in mV"].copy(), 340, 5)
+
+    # EKG-Daten als Matplotlib Plot anzeigen
+    # Nachdem die EKG, Daten geladen wurden
+    # Erstelle den Plot als Attribut des Objektes
+    fig = ekgdata.EKGdata.make_ekg_plot(peaks, df)
+    # Zeige den Plot an
+    st.plotly_chart(fig)
+
+    # Plot the heart rate
+    heart_rate_times, heart_rate_at_peaks = ekgdata.EKGdata.estimate_hr(peaks)
+
+    #print(heart_rate_times)
+    #print('---')
+    #print(current_ekg)
+
+
+    fig2 = ekgdata.EKGdata.make_hf_plot(heart_rate_times, heart_rate_at_peaks)
+    st.plotly_chart(fig2)
+
+
+else:
+    st.write("Keine EKG-Daten vorhanden.")
