77f2948f4b4e8642278a082048d1ed7115e91c71
svenjamariko <svenja.taft@icloud.com>
2024-06-10 12:00:47 +0200

    svenja: artikel, abourus,impressum

---
+++ impressum.py
@@ -0,0 +1,30 @@
+# impressum.py
+import streamlit as st
+
+def impressum():
+    st.title('Impressum')
+    st.write("""
+    ### Kontaktinformationen
+
+    **Management Center Innsbruck (MCI)**
+    
+    Adresse: Universitätsstraße 15, 6020 Innsbruck, Österreich
+    Telefon: +43 512 2070-0
+    E-Mail: office@mci.edu
+    Website: [MCI Website](https://www.mci.edu)
+
+    ### Unser Team
+
+    - **Felicitas Thierbach**
+      - E-Mail: 
+    
+    - **Svenja Taft**
+      - E-Mail: ts7312@mci4me.at
+    
+    - **Rania Shehata**
+      - E-Mail: rania.shehata@example.com
+
+    ### Haftungsausschluss
+
+    Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+    """)
